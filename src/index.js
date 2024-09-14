c258b8ae2acecaa3b31aa63bbb7419a9b0f6a3b6
weigucheng <weigucheng@i-hc.cn>
2024-09-14 15:53:56 +0800

    修改缩进

--- src/index.js
+++ src/index.js
@@ -1,271 +1,271 @@
 function _parse (marked, input) {
-  try {
-    let html = marked.parse(input);
-    html = `<div class="modell-markedjs-plus-box"><div>${html}</div></div>`;
-    return html;
-  } catch (err) {
-    console.log(err);
-    return null;
-  }
+	try {
+		let html = marked.parse(input);
+		html = `<div class="modell-markedjs-plus-box"><div>${html}</div></div>`;
+		return html;
+	} catch (err) {
+		console.log(err);
+		return null;
+	}
 }
 
 function create (marked) {
 
-  const customExtensions = [];
-  const imageMap = {}, tableMap = {}, levelMap = {}, anchorMap = {};
-  let tableIndex = 1, imgIndex = 1, anchorIndex = 1;
-  let levelIndex = [ 0, 0, 0, 0, 0, 0 ], lastLevel = 0;
-  let fileUrl = "", imgDefaultAlign = "left";
-  let _highlight = (code) => {
-    return code;
-  }
+	const customExtensions = [];
+	const imageMap = {}, tableMap = {}, levelMap = {}, anchorMap = {};
+	let tableIndex = 1, imgIndex = 1, anchorIndex = 1;
+	let levelIndex = [ 0, 0, 0, 0, 0, 0 ], lastLevel = 0;
+	let fileUrl = "", imgDefaultAlign = "left";
+	let _highlight = (code) => {
+		return code;
+	}
 
-  const rendererMD = new marked.Renderer();
-  const lexer = new marked.Lexer();
-  const parser = new marked.Parser();
+	const rendererMD = new marked.Renderer();
+	const lexer = new marked.Lexer();
+	const parser = new marked.Parser();
 
-  rendererMD.heading = function(text, level, raw) {
-    if (lastLevel > level) {
-      for (let i = level; i < levelIndex.length; i++) {
-        levelIndex[i] = 0;
-      }
-    }
+	rendererMD.heading = function(text, level, raw) {
+		if (lastLevel > level) {
+			for (let i = level; i < levelIndex.length; i++) {
+				levelIndex[i] = 0;
+			}
+		}
 
-    levelIndex[level - 1]++;
+		levelIndex[level - 1]++;
 
-    const chapter = levelIndex.slice(0, level).join(".");
+		const chapter = levelIndex.slice(0, level).join(".");
 
-    levelMap[text.trim()] = chapter;
-    lastLevel = level;
+		levelMap[text.trim()] = chapter;
+		lastLevel = level;
 
-    const output = `<p class="doc-heading">${chapter}. ${text}</p>`;
-    return output;
-  }
-  rendererMD.link = function(href, title, text) {
-    text = text || href;
-    title = text;
+		const output = `<p class="doc-heading">${chapter}. ${text}</p>`;
+		return output;
+	}
+	rendererMD.link = function(href, title, text) {
+		text = text || href;
+		title = text;
 
-    let html = `<a href="${href}">${text}</a>`;
-    return html;
-  };
+		let html = `<a href="${href}">${text}</a>`;
+		return html;
+	};
 
-  rendererMD.image = function(href, title, text) {
-    if (text) {
-      text = `：${text}`;
-    }
+	rendererMD.image = function(href, title, text) {
+		if (text) {
+			text = `：${text}`;
+		}
 
-    let [ _href, align ] = href.split("|");
-    href = _href;
-    align = align || imgDefaultAlign;
+		let [ _href, align ] = href.split("|");
+		href = _href;
+		align = align || imgDefaultAlign;
 
-    let index = imageMap[href];
-    if (!index) {
-      index = imgIndex++;
-      imageMap[href] = index;
-    }
-    const html = `<div class="doc-img obj-align__${align}"><img id="#p${index}" src="${fileUrl}${href}" /><div>图 ${index}${text}</div></div>`;
-    return html;
-  }
+		let index = imageMap[href];
+		if (!index) {
+			index = imgIndex++;
+			imageMap[href] = index;
+		}
+		const html = `<div class="doc-img obj-align__${align}"><img id="#p${index}" src="${fileUrl}${href}" /><div>图 ${index}${text}</div></div>`;
+		return html;
+	}
 
-  rendererMD.code = function (code, info, escaped) {
-    info = info.replace("(", "");
-    info = info.replace(")", "");
-    const value = _highlight(code, info, escaped);
+	rendererMD.code = function (code, info, escaped) {
+		info = info.replace("(", "");
+		info = info.replace(")", "");
+		const value = _highlight(code, info, escaped);
 
-    return `<pre class="doc-code"><code class="language-html">${value}</code></pre>`;
-  };
+		return `<pre class="doc-code"><code class="language-html">${value}</code></pre>`;
+	};
 
-  rendererMD.table = function(thead, tbody) {
-    const html = `<div class="doc-table"><table>${thead}${tbody}</table></div>`;
-    return html;
-  }
-  rendererMD.strong = function(value) {
-    const arr = value.split("$");
-    let html = `<span class="span-bold`;
-    for (let i = 1; i < arr.length; i++) {
-      const part = arr[i];
-      html += ` span-${part}`;
-    }
+	rendererMD.table = function(thead, tbody) {
+		const html = `<div class="doc-table"><table>${thead}${tbody}</table></div>`;
+		return html;
+	}
+	rendererMD.strong = function(value) {
+		const arr = value.split("$");
+		let html = `<span class="span-bold`;
+		for (let i = 1; i < arr.length; i++) {
+			const part = arr[i];
+			html += ` span-${part}`;
+		}
 
-    html = `${html}">${arr[0]}</span>`;
+		html = `${html}">${arr[0]}</span>`;
 
-    return html;
-  }
+		return html;
+	}
 
-  const extractsAt = {
-    name: "extractsAt",
-    level: "inline",
-    start: (src) => {
-      const match = /@\[(image|icon|table|anchor)\]\{(\S+?)\}/.exec(src);
-      if (match) {
-        return match.index;
-      } else {
-        return -1;
-      }
-    },
-    tokenizer: (src, tokens) => {
-      const match = /@\[(image|icon|table|anchor)\]\{(\S+?)\}/.exec(src);
-      if (match) {
-        let [ raw, kind, value ] = match;
-        const text = raw;
-        const index = src.indexOf(raw);
-        raw = src.slice(0, index) + raw;
+	const extractsAt = {
+		name: "extractsAt",
+		level: "inline",
+		start: (src) => {
+			const match = /@\[(image|icon|table|anchor)\]\{(\S+?)\}/.exec(src);
+			if (match) {
+				return match.index;
+			} else {
+				return -1;
+			}
+		},
+		tokenizer: (src, tokens) => {
+			const match = /@\[(image|icon|table|anchor)\]\{(\S+?)\}/.exec(src);
+			if (match) {
+				let [ raw, kind, value ] = match;
+				const text = raw;
+				const index = src.indexOf(raw);
+				raw = src.slice(0, index) + raw;
 
-        const token = {
-          type: "extractsAt",
-          raw, value, text, kind,
-          tokens:  lexer.inline(text, tokens)
-        }
+				const token = {
+					type: "extractsAt",
+					raw, value, text, kind,
+					tokens:	lexer.inline(text, tokens)
+				}
 
-        return token;
-      }
-    },
-    renderer ({ raw, value, text, kind }) {
-      let output = null;
-      switch (kind) {
-        case "image":
-          let index1 = imageMap[value];
-          if (!index1) {
-            index1 = imgIndex++;
-            imageMap[value] = index1;
-          }
+				return token;
+			}
+		},
+		renderer ({ raw, value, text, kind }) {
+			let output = null;
+			switch (kind) {
+				case "image":
+					let index1 = imageMap[value];
+					if (!index1) {
+						index1 = imgIndex++;
+						imageMap[value] = index1;
+					}
 
-          output = `<span class="span-bold">图 ${index1}</span>`;
-          break;
-        case "icon":
-          value = value.replace(",", " ");
-          output = `<i class="span-bold ${value}"></i>`;
-          break;
-        case "table":
-          const isTable = value.indexOf(":") < 0;
+					output = `<span class="span-bold">图 ${index1}</span>`;
+					break;
+				case "icon":
+					value = value.replace(",", " ");
+					output = `<i class="span-bold ${value}"></i>`;
+					break;
+				case "table":
+					const isTable = value.indexOf(":") < 0;
 
-          if (!isTable) {
-            value = value.slice(1);
-          }
+					if (!isTable) {
+						value = value.slice(1);
+					}
 
-          let index2 = tableMap[value];
-          if (!index2) {
-            index2 = tableIndex++;
-            tableMap[value] = index2;
-          }
+					let index2 = tableMap[value];
+					if (!index2) {
+						index2 = tableIndex++;
+						tableMap[value] = index2;
+					}
 
-          if (isTable) {
-            return `<div class="obj-align__center span-bold">表 ${index2}：${value}</div>`; // 在 输入为 @X 的时候，表示这里有个表格，不做任何处理
-          } else {
-            output = `<span class="span-bold">表 ${index2}</span>`;
-          }
+					if (isTable) {
+						return `<div class="obj-align__center span-bold">表 ${index2}：${value}</div>`; // 在 输入为 @X 的时候，表示这里有个表格，不做任何处理
+					} else {
+						output = `<span class="span-bold">表 ${index2}</span>`;
+					}
 
-          break;
-        case "anchor":
-          const isAnchor = value.indexOf(":") < 0;
+					break;
+				case "anchor":
+					const isAnchor = value.indexOf(":") < 0;
 
-          if (isAnchor) {
-            if (anchorMap[value]) return; // 不知道为什么，这里会调用两次，所以去重
-            output = `<span id="a_${anchorIndex}">${value}</span>`;
-            anchorMap[value] = anchorIndex++;
-          } else {
-            value = value.slice(1);
-            const index = anchorMap[value];
-            output = `<a href="#a_${index}">${value}</a>`;
-          }
-      }
+					if (isAnchor) {
+						if (anchorMap[value]) return; // 不知道为什么，这里会调用两次，所以去重
+						output = `<span id="a_${anchorIndex}">${value}</span>`;
+						anchorMap[value] = anchorIndex++;
+					} else {
+						value = value.slice(1);
+						const index = anchorMap[value];
+						output = `<a href="#a_${index}">${value}</a>`;
+					}
+			}
 
-      raw = raw.replace(text, output);
-      raw = raw.replace("  ", "<br />"); // TODO 这里是否只有这个问题还要再看
-      return raw;
-    }
-  };
+			raw = raw.replace(text, output);
+			raw = raw.replace("	", "<br />"); // TODO 这里是否只有这个问题还要再看
+			return raw;
+		}
+	};
 
-  const extractsColor = {
-    name: "extractsColor",
-    level: "inline",
-    start: (src) => {
-      const match = /#\[([0-9a-fA-F]{6})\]\{([\S\s]+?)\}/.exec(src);
-      if (match) {
-        return match.index;
-      } else {
-        return -1;
-      }
-    },
-    tokenizer: (src, tokens) => {
-      const match = /#\[([0-9a-fA-F]{6})\]\{([\S\s]+?)\}/.exec(src);
-      if (match) {
-        let [ raw, color, text ] = match;
-        const value = raw;
-        const index = src.indexOf(raw);
-        raw = src.slice(0, index) + raw;
+	const extractsColor = {
+		name: "extractsColor",
+		level: "inline",
+		start: (src) => {
+			const match = /#\[([0-9a-fA-F]{6})\]\{([\S\s]+?)\}/.exec(src);
+			if (match) {
+				return match.index;
+			} else {
+				return -1;
+			}
+		},
+		tokenizer: (src, tokens) => {
+			const match = /#\[([0-9a-fA-F]{6})\]\{([\S\s]+?)\}/.exec(src);
+			if (match) {
+				let [ raw, color, text ] = match;
+				const value = raw;
+				const index = src.indexOf(raw);
+				raw = src.slice(0, index) + raw;
 
-        const token = {
-          type: "extractsColor",
-          proto: value,
-          raw, color, text,
-          tokens: lexer.inlineTokens(text)
-        }
+				const token = {
+					type: "extractsColor",
+					proto: value,
+					raw, color, text,
+					tokens: lexer.inlineTokens(text)
+				}
 
-        return token;
-      }
-    },
-    renderer ({raw, proto, tokens, color}) {
-      const value = parser.parseInline(tokens);
-      const output = `<span style="color:#${color}">${value}</span>`;
-      raw = raw.replace(proto, output);
-      raw = raw.replace("  ", "<br />"); // TODO 这里是否只有这个问题还要再看
-      return raw;
-    }
-  };
+				return token;
+			}
+		},
+		renderer ({raw, proto, tokens, color}) {
+			const value = parser.parseInline(tokens);
+			const output = `<span style="color:#${color}">${value}</span>`;
+			raw = raw.replace(proto, output);
+			raw = raw.replace("	", "<br />"); // TODO 这里是否只有这个问题还要再看
+			return raw;
+		}
+	};
 
-  marked.setOptions({
-    renderer: rendererMD,
-    lexer: lexer,
-    parser: parser
-  });
+	marked.setOptions({
+		renderer: rendererMD,
+		lexer: lexer,
+		parser: parser
+	});
 
-  return {
-    parse: (file) => {
-        const extensions = [extractsAt, extractsColor].concat(customExtensions);
+	return {
+		parse: (file) => {
+				const extensions = [extractsAt, extractsColor].concat(customExtensions);
 
-        marked.use({ extensions });
-    
-        return _parse(marked, file);
+				marked.use({ extensions });
+		
+				return _parse(marked, file);
 
-    },
-    addCustomExtension (obj) {
-      customExtensions.push(obj);
-    },
-    getLexer () {
-      return lexer;
-    },
-    getParser () {
-      return parser;
-    },
-    getRenderer () {
-      return rendererMD;
-    },
-    codeHighlight (highlight) {
-      _highlight = highlight;
-    },
-    setFileDefaultUrl (url) {
-      fileUrl = url;
-      if (url[url.length - 1] !== "/") {
-        fileUrl += "/";
-      }
-    },
-    setImageDefaultAlign (align) {
-      imgDefaultAlign = align;
-    }
-  }
+		},
+		addCustomExtension (obj) {
+			customExtensions.push(obj);
+		},
+		getLexer () {
+			return lexer;
+		},
+		getParser () {
+			return parser;
+		},
+		getRenderer () {
+			return rendererMD;
+		},
+		codeHighlight (highlight) {
+			_highlight = highlight;
+		},
+		setFileDefaultUrl (url) {
+			fileUrl = url;
+			if (url[url.length - 1] !== "/") {
+				fileUrl += "/";
+			}
+		},
+		setImageDefaultAlign (align) {
+			imgDefaultAlign = align;
+		}
+	}
 }
 
 module.exports = exports = (Marked) => {
-  return {
-    create: () => {
-      return create(new Marked());
-    },
-    parse (file) {
-      const marked = new Marked();
-      return _parse(marked, file);
-    }
-  }
+	return {
+		create: () => {
+			return create(new Marked());
+		},
+		parse (file) {
+			const marked = new Marked();
+			return _parse(marked, file);
+		}
+	}
 };
 
 
